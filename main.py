c94b64e76bc5281a068f140a07ab9835b839001f
Anton Petrov <a.petrov.83@gmail.com>
2026-08-06 11:05:53 +0300

    Create a class for file

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 import os
 import subprocess
 
+from my_file.file import File
+
+COMMAND: list[str] = ["cmd", "/c", "cls"]
+
 def main():
-    command = ["cmd", "/c", "cls"]
-    subprocess.run(command)
-    total_files = 0
-    renamed_files = 0
+    # Clear the screen when started
+    subprocess.run(COMMAND)
 
-    ask_for = True
-    answer = input("Do you want to confirm each rename? (Y/n): ")
+    # Variables to keep the values of total files and renamed files.
+    total_files: int = 0
+    renamed_files: int = 0
+
+    # Ask user if wants to be asked for each renaming (IMPORT: if files are many it will be annoying)
+    is_interactive: bool = True
+    answer: str = input("Do you want to confirm each rename? (Y/n): ")
     if answer.lower() == 'n':
-        ask_for = False
+        is_interactive = False
 
+    # Request for user where files are located
     dir = str(input("Directory: "))
 
+    # List of all paths 
     paths = (os.path.join(root, file_namename)
             for root, _, file_namenames in os.walk(dir)
             for file_namename in file_namenames)
 
     for path in paths:
         total_files +=1
-        old_path = path
-        file_name = old_path.split('\\')[-1]
-        path = '\\'.join(old_path.split('\\')[:-1])
+        curr_file: File = File(path)
+        new_path: str = curr_file.new_path()
         
-        file_name = file_name.lower()
-        file_name = file_name.replace('. ', '-')
-        file_name = file_name.replace(' ', '-')
-        
-        new_path = path + '\\'+file_name
-        
-        if old_path != new_path:
-            if ask_for:
+        if path != new_path:
+            if is_interactive:
                 print('-' * 100)
-                print(old_path)
-                print(new_path)
-                curr_rename = input("Rename this file (Y/n): ")
+                print(f"Old NAME:   {curr_file.old_file_name()}")
+                print(f"New NAME:   {curr_file.new_file_name()}")
+                ask_for_curr_rename: str = input("Do you want to rename this file (Y/n) or (q) for quit: ")
 
-                if curr_rename.lower() == 'y':
+                if ask_for_curr_rename.lower() == 'q':
+                    return
+
+                if ask_for_curr_rename.lower() == 'y':
                     renamed_files += 1
-                    os.rename(old_path, new_path)
+                    curr_file.rename()
                     
             else:
                 renamed_files += 1
-                os.rename(old_path, new_path)
-
+                curr_file.rename()
 
     print("=" * 100)
     print(f"Total files: {total_files}")
