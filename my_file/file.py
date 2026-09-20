2b7230aa0e02b9cf22f76a95c0c00854eb4c9807
Anton Petrov <a.petrov.83@gmail.com>
2026-09-20 11:58:57 +0300

    add more conditions

--- my_file/file.py
+++ my_file/file.py
@@ -1,26 +1,30 @@
 import os
 
+
 class File:
     def __init__(self, initinal_path: str) -> None:
-        self.initinal_path:str = initinal_path
+        self.initinal_path: str = initinal_path
 
     def old_file_name(self) -> str:
-        file_name: str = self.initinal_path.split('\\')[-1]
+        file_name: str = self.initinal_path.split("\\")[-1]
 
         return file_name
 
     def new_file_name(self) -> str:
-        file_name: str = self.initinal_path.split('\\')[-1]
+        file_name: str = self.initinal_path.split("\\")[-1]
         file_name = file_name.lower()
-        file_name = file_name.replace('. ', '-')
-        file_name = file_name.replace(' ', '-')
-        
+        file_name = file_name.replace(", ", " ")
+        file_name = file_name.replace(". ", "-")
+        file_name = file_name.replace(" ", "-")
+        file_name = file_name.replace("+", "-")
+        file_name = file_name.replace("----", "---")
+
         return file_name
 
     def new_path(self) -> str:
-        path: str = '\\'.join(self.initinal_path.split('\\')[:-1])
-        path += '\\' + self.new_file_name()
-        
+        path: str = "\\".join(self.initinal_path.split("\\")[:-1])
+        path += "\\" + self.new_file_name()
+
         return path
 
     def rename(self) -> None:
